3fbe8c08be28931c4d340c49f2e216f13caf32ab
denchic45 <denchic150@gmail.com>
2025-10-20 16:45:15 +0300

    Update errors in auth

--- backend/src/main/kotlin/com/denchic45/financetracker/feature/auth/AuthService.kt
+++ backend/src/main/kotlin/com/denchic45/financetracker/feature/auth/AuthService.kt
@@ -3,15 +3,14 @@ package com.denchic45.financetracker.feature.auth
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
-import com.denchic45.financetracker.auth.AuthErrors
 import com.denchic45.financetracker.auth.model.SignInRequest
 import com.denchic45.financetracker.auth.model.SignUpRequest
 import com.denchic45.financetracker.database.table.UserDao
 import com.denchic45.financetracker.database.table.Users
-import com.denchic45.financetracker.feature.error.DomainError
-import com.denchic45.financetracker.feature.error.EmailAlreadyUsed
-import com.denchic45.financetracker.feature.error.WrongEmail
-import io.ktor.server.plugins.*
+import com.denchic45.financetracker.error.DomainError
+import com.denchic45.financetracker.error.EmailAlreadyUsed
+import com.denchic45.financetracker.error.WrongEmail
+import com.denchic45.financetracker.error.WrongPassword
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.mindrot.jbcrypt.BCrypt
@@ -33,11 +32,11 @@ class AuthService {
     }
 
 
-    fun findUserId(request: SignInRequest): Either<WrongEmail, Long> = transaction {
+    fun findUserId(request: SignInRequest): Either<DomainError, UUID> = transaction {
         val user = UserDao.find(Users.email eq request.email).singleOrNull()
             ?: return@transaction WrongEmail.left()
 
-        if (!BCrypt.checkpw(request.password, user.password)) throw BadRequestException(AuthErrors.INVALID_PASSWORD)
-        user.id.value.right()
+        if (!BCrypt.checkpw(request.password, user.password)) WrongPassword.left()
+        else user.id.value.right()
     }
 }
