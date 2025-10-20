8608fd413b9c572fb761118a31fa1f8c975dbcf9
denchic45 <denchic150@gmail.com>
2025-10-20 16:36:35 +0300

    Remove old AuthErrors

--- common/api/src/main/kotlin/com/denchic45/financetracker/auth/AuthErrors.kt
+++
@@ -1,7 +0,0 @@
-package com.denchic45.financetracker.auth
-
-object AuthErrors {
-    const val USER_NOT_EXIST = "NO_EXIST_LOGIN"
-    const val INVALID_PASSWORD = "INVALID_PASSWORD"
-    const val USER_EMAIL_ALREADY_USED = "USER_EMAIL_ALREADY_USED"
-}
