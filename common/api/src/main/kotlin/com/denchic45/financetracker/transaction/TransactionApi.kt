f41177da2c409b1f6e43e227ae5ff302959bcc83
Denis <denchic150@gmail.com>
2025-10-26 22:58:44 +0300

    Move TransactionApi

---
+++ common/api/src/main/kotlin/com/denchic45/financetracker/transaction/TransactionApi.kt
@@ -0,0 +1,58 @@
+package com.denchic45.financetracker.transaction
+
+import com.denchic45.financetracker.response.ResponseResult
+import com.denchic45.financetracker.response.EmptyResponseResult
+import com.denchic45.financetracker.response.safeApiCall
+import com.denchic45.financetracker.response.safeApiCallForEmpty
+import com.denchic45.financetracker.transaction.model.TransactionRequest
+import com.denchic45.financetracker.transaction.model.TransactionResponse
+import com.denchic45.financetracker.util.PagingResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.get
+import io.ktor.client.request.parameter
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+
+class TransactionApi(private val client: HttpClient) {
+
+    suspend fun add(request: TransactionRequest): ResponseResult<TransactionResponse> {
+        return client.post("/transactions") {
+            contentType(ContentType.Application.Json)
+            setBody(request)
+        }.body()
+    }
+
+    suspend fun getList(page: Int, pageSize: Int): ResponseResult<PagingResponse<TransactionResponse>> {
+        return client.safeApiCall {
+            get("/transactions") {
+                parameter("page", page)
+                parameter("pageSize", pageSize)
+            }
+        }
+    }
+
+    suspend fun getById(transactionId: Long): ResponseResult<TransactionResponse> {
+        return client.safeApiCall { get("/transactions/$transactionId") }
+    }
+
+    suspend fun update(
+        transactionId: Long,
+        request: TransactionRequest
+    ): ResponseResult<TransactionResponse> {
+        return client.safeApiCall {
+            put("/transactions/$transactionId") {
+                contentType(ContentType.Application.Json)
+                setBody(request)
+            }
+        }
+    }
+
+    suspend fun delete(transactionId: Long): EmptyResponseResult {
+        return client.safeApiCallForEmpty { delete("/transactions/$transactionId") }
+    }
+}
