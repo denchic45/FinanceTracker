7ec343dc59ebe5c39775915d38bfd5bd43fcfda2
Denis <denchic150@gmail.com>
2025-10-26 23:40:06 +0300

    Update TransactionRemoteMediator

--- app/app/src/main/java/com/denchic45/financetracker/data/TransactionRemoteMediator.kt
+++ app/app/src/main/java/com/denchic45/financetracker/data/TransactionRemoteMediator.kt
@@ -5,12 +5,12 @@ import androidx.paging.LoadType
 import androidx.paging.PagingState
 import androidx.paging.RemoteMediator
 import androidx.room.withTransaction
+import arrow.core.getOrElse
 import com.denchic45.financetracker.data.database.AppDatabase
 import com.denchic45.financetracker.data.database.entity.AggregatedTransactionEntity
 import com.denchic45.financetracker.data.mapper.toTransactionEntities
-import com.denchic45.financetracker.transaction.model.TransactionApi
-import com.denchic45.financetracker.util.ApiException
-import kotlinx.io.IOException
+import com.denchic45.financetracker.response.asThrowable
+import com.denchic45.financetracker.transaction.TransactionApi
 
 @OptIn(ExperimentalPagingApi::class)
 class TransactionRemoteMediator(
@@ -24,32 +24,28 @@ class TransactionRemoteMediator(
         loadType: LoadType,
         state: PagingState<Int, AggregatedTransactionEntity>
     ): MediatorResult {
-        return try {
-            page = when (loadType) {
-                LoadType.REFRESH -> 1
-                LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
-                LoadType.APPEND -> {
-                    val lastItem = state.lastItemOrNull()
+        page = when (loadType) {
+            LoadType.REFRESH -> 1
+            LoadType.PREPEND -> return MediatorResult.Success(endOfPaginationReached = true)
+            LoadType.APPEND -> {
+                val lastItem = state.lastItemOrNull()
 
-                    if (lastItem == null) 1
-                    else page + 1
-                }
+                if (lastItem == null) 1
+                else page + 1
             }
-            val transactions = transactionApi.getList(page, 30)
-
-            val transactionDao = database.transactionDao
-            database.withTransaction {
-                if (loadType == LoadType.REFRESH) {
-                    transactionDao.deleteAll()
-                }
-                transactionDao.upsert(transactions.results.toTransactionEntities())
-            }
-            MediatorResult.Success(transactions.info.next == null)
-
-        } catch (exception: IOException) {
-            MediatorResult.Error(exception)
-        } catch (exception: ApiException) {
-            MediatorResult.Error(exception)
         }
+        val transactions = transactionApi.getList(page, 30)
+            .getOrElse {
+                return MediatorResult.Error(it.asThrowable())
+            }
+
+        val transactionDao = database.transactionDao
+        database.withTransaction {
+            if (loadType == LoadType.REFRESH) {
+                transactionDao.deleteAll()
+            }
+            transactionDao.upsert(transactions.results.toTransactionEntities())
+        }
+        return MediatorResult.Success(transactions.info.next == null)
     }
 }
