7e07756bf0b0454a4cf688d86d698afcac3a7a89
Denis <denchic150@gmail.com>
2025-10-26 23:44:59 +0300

    Implement update token

--- app/app/src/main/java/com/denchic45/financetracker/data/AppPreferences.kt
+++ app/app/src/main/java/com/denchic45/financetracker/data/AppPreferences.kt
@@ -3,15 +3,19 @@ package com.denchic45.financetracker.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.map
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
-class AppPreferences(dataStore: DataStore<Preferences>) {
+class AppPreferences(private val dataStore: DataStore<Preferences>) {
     private val tokenKey = stringPreferencesKey("token")
 
     val token = dataStore.data.map { it[tokenKey] }
 
+    suspend fun setToken(token: String) {
+        dataStore.edit { it[tokenKey] = token }
+    }
 }
