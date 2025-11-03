b9abe3ed6a8fabdc0c055780a10ed8be7a6beb67
denchic45 <denchic150@gmail.com>
2025-11-03 21:13:01 +0300

    add expenseSum and incomeSum for statistics

--- backend/src/main/kotlin/com/denchic45/financetracker/database/table/Transactions.kt
+++ backend/src/main/kotlin/com/denchic45/financetracker/database/table/Transactions.kt
@@ -5,16 +5,30 @@ import org.jetbrains.exposed.dao.LongEntity
 import org.jetbrains.exposed.dao.LongEntityClass
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.dao.id.LongIdTable
-import org.jetbrains.exposed.sql.javatime.datetime
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.case
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.kotlin.datetime.datetime
+import java.util.*
 
 object Transactions : LongIdTable("transaction", "transaction_id") {
     val datetime = datetime("datetime")
     val amount = long("amount")
     val type = enumerationByName<TransactionType>("transaction_type", 28)
     val description = text("description")
-    val accountId = reference("source_id", Accounts)
+    val sourceAccountId = reference("source_account_id", Accounts)
     val categoryId = optReference("category_id", Categories)
-    val incomeAccountId = optReference("income_account", Accounts)
+    val incomeAccountId = optReference("income_account_id", Accounts)
+
+    val expenseSum = case()
+        .When(type eq TransactionType.EXPENSE, amount)
+        .Else(longLiteral(0))
+        .sum()
+
+    val incomeSum = case()
+        .When(type eq TransactionType.INCOME, amount)
+        .Else(longLiteral(0))
+        .sum()
 }
 
 class TransactionDao(id: EntityID<Long>) : LongEntity(id) {
@@ -24,7 +38,7 @@ class TransactionDao(id: EntityID<Long>) : LongEntity(id) {
     var amount by Transactions.amount
     var type by Transactions.type
     var description by Transactions.description
-    var account by AccountDao referencedOn Transactions.accountId
+    var account by AccountDao referencedOn Transactions.sourceAccountId
     var category by CategoryDao optionalReferencedOn Transactions.categoryId
     var incomeAccount by AccountDao optionalReferencedOn Transactions.incomeAccountId
     var tags by TagDao via TransactionTags
