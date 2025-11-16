cd6b16fac0672cde0f6ac8bd1002ea1f5bd6575e
denchic45 <denchic150@gmail.com>
2025-11-16 22:50:45 +0300

    Add repositories for Category and Tag, and StatisticsService

--- app/app/src/main/java/com/denchic45/financetracker/di/DataModule.kt
+++ app/app/src/main/java/com/denchic45/financetracker/di/DataModule.kt
@@ -3,17 +3,23 @@ package com.denchic45.financetracker.di
 import android.content.Context
 import androidx.room.Room
 import com.denchic45.financetracker.data.AppPreferences
-import com.denchic45.financetracker.data.AuthService
 import com.denchic45.financetracker.data.dataStore
 import com.denchic45.financetracker.data.database.AppDatabase
 import com.denchic45.financetracker.data.repository.AccountRepository
+import com.denchic45.financetracker.data.repository.CategoryRepository
+import com.denchic45.financetracker.data.repository.TagRepository
 import com.denchic45.financetracker.data.repository.TransactionRepository
+import com.denchic45.financetracker.data.service.AuthService
+import com.denchic45.financetracker.data.service.StatisticsService
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val repositoryModule = module {
     singleOf(::AccountRepository)
     singleOf(::TransactionRepository)
+    singleOf(::CategoryRepository)
+    singleOf(::TagRepository)
+    singleOf(::StatisticsService)
 }
 
 val databaseModule = module {
@@ -27,6 +33,7 @@ val databaseModule = module {
     single { get<AppDatabase>().accountDao }
     single { get<AppDatabase>().categoryDao }
     single { get<AppDatabase>().transactionDao }
+    single { get<AppDatabase>().tagDao }
 }
 
 val dataModule = module {
