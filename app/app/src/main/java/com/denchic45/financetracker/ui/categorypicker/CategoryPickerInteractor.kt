935d64e050f2f4d1f1b2ea4ad7ac7765888a44b3
denchic45 <denchic150@gmail.com>
2025-11-16 22:33:51 +0300

    Implement category picker

---
+++ app/app/src/main/java/com/denchic45/financetracker/ui/categorypicker/CategoryPickerInteractor.kt
@@ -0,0 +1,6 @@
+package com.denchic45.financetracker.ui.categorypicker
+
+import com.denchic45.financetracker.domain.model.CategoryItem
+import com.denchic45.financetracker.ui.PickerInteractor
+
+class CategoryPickerInteractor: PickerInteractor<CategoryItem>()
---
+++ app/app/src/main/java/com/denchic45/financetracker/ui/categorypicker/CategoryPickerViewModel.kt
@@ -0,0 +1,34 @@
+package com.denchic45.financetracker.ui.categorypicker
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.denchic45.financetracker.di.AppRouter
+import com.denchic45.financetracker.domain.model.CategoryItem
+import com.denchic45.financetracker.domain.usecase.ObserveCategoriesUseCase
+import com.denchic45.financetracker.ui.main.NavEntry
+import com.denchic45.financetracker.ui.navigation.router.pop
+import com.denchic45.financetracker.ui.navigation.router.push
+import com.denchic45.financetracker.ui.resource.stateInCacheableResource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.launch
+
+class CategoryPickerViewModel(
+    val income: Boolean,
+    private val categoryPickerInteractor: CategoryPickerInteractor,
+    observeCategoriesUseCase: ObserveCategoriesUseCase,
+    private val router: AppRouter
+) : ViewModel() {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    val categories = observeCategoriesUseCase(income)
+        .stateInCacheableResource(viewModelScope)
+
+    fun onCreateCategoryClick() = router.push(NavEntry.CategoryEditor(null, income))
+
+    fun onSelect(category: CategoryItem) {
+        router.pop()
+        viewModelScope.launch { categoryPickerInteractor.onSelect(category) }
+    }
+
+    fun onDismissClick() = router.pop()
+}
