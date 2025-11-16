b97e1aec1131c9f0041facb36d4c3e63738a6e82
denchic45 <denchic150@gmail.com>
2025-11-16 22:39:17 +0300

    Refactor AccountEditor to a full screen
    - Replace the `AccountEditorDialog` with a full-screen `AccountEditorScreen`.
    - Implement form validation for the account name and initial balance fields.
    - Add logic to handle both creating new accounts and updating existing ones.
    - Introduce a confirmation dialog for discarding unsaved changes.

--- app/app/src/main/java/com/denchic45/financetracker/ui/accounteditor/AccountEditorUI.kt
+++ app/app/src/main/java/com/denchic45/financetracker/ui/accounteditor/AccountEditorUI.kt
@@ -1,51 +1,96 @@
 package com.denchic45.financetracker.ui.accounteditor
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuAnchorType
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.FilledTonalButton
-import androidx.compose.material3.MenuAnchorType
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 import com.denchic45.financetracker.api.account.model.AccountType
+import com.denchic45.financetracker.ui.CurrencyVisualTransformation
 import org.koin.compose.viewmodel.koinViewModel
 import org.koin.core.parameter.parametersOf
 import java.util.UUID
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AccountEditorDialog(
+fun AccountEditorScreen(
     accountId: UUID?,
-    onFinish: () -> Unit
-) {
-    val viewModel = koinViewModel<AccountEditorViewModel> {
+    viewModel: AccountEditorViewModel = koinViewModel<AccountEditorViewModel> {
         parametersOf(accountId)
     }
+) {
     val state = viewModel.state
 
-    var showRemoveConfirmation by remember { mutableStateOf(false) }
-    AlertDialog(
-        title = { Text(if (accountId != null) "Редактировать аккаунт" else "Создать аккаунт") },
-        text = {
+    var showDiscardConfirmation by remember { mutableStateOf(false) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(if (accountId != null) "Редактировать аккаунт" else "Создать аккаунт") },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        if (viewModel.hasChanges()) showDiscardConfirmation = true
+                        else viewModel.onDismissClick()
+                    }) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Назад"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = viewModel::onSaveClick) {
+                        Icon(imageVector = Icons.Default.Check, contentDescription = "Сохранить")
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = state.name,
+                onValueChange = viewModel::onAccountNameChanged,
+                label = { Text("Название") },
+                modifier = Modifier.fillMaxWidth(),
+                isError = state.nameMessage != null,
+                supportingText = { state.nameMessage?.let { Text(it) } }
+            )
             var accountTypesExpanded by remember { mutableStateOf(false) }
             ExposedDropdownMenuBox(
-                expanded = accountTypesExpanded, onExpandedChange = { accountTypesExpanded = it }) {
+                expanded = accountTypesExpanded,
+                onExpandedChange = { accountTypesExpanded = it }) {
                 OutlinedTextField(
                     modifier = Modifier
                         .fillMaxWidth()
-                        .menuAnchor(MenuAnchorType.PrimaryEditable),
+                        .menuAnchor(ExposedDropdownMenuAnchorType.PrimaryEditable),
                     value = state.type.displayName,
                     onValueChange = { },
                     readOnly = true,
@@ -54,12 +99,12 @@ fun AccountEditorDialog(
                     trailingIcon = {
                         ExposedDropdownMenuDefaults.TrailingIcon(
                             expanded = accountTypesExpanded,
-                            modifier = Modifier.menuAnchor(MenuAnchorType.SecondaryEditable),
+                            modifier = Modifier.menuAnchor(ExposedDropdownMenuAnchorType.SecondaryEditable),
                         )
                     },
-                    colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                    colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
                     supportingText = {})
-                DropdownMenu(
+                ExposedDropdownMenu(
                     expanded = accountTypesExpanded,
                     onDismissRequest = { accountTypesExpanded = false }) {
                     AccountType.entries.forEach {
@@ -70,44 +115,41 @@ fun AccountEditorDialog(
                     }
                 }
             }
-            OutlinedTextField(
-                value = state.name,
-                onValueChange = viewModel::onAccountNameChanged,
-                modifier = Modifier.fillMaxWidth()
-            )
-        },
-        confirmButton = {
-            FilledTonalButton(onClick = viewModel::onSaveClick) { Text("Сохранить") }
-        },
-        dismissButton = {
-            TextButton(onClick = {
-                if (viewModel.hasChanges()) showRemoveConfirmation = true
-                else onFinish()
-            }) { Text("Отмена") }
-        },
-        onDismissRequest = {
-            if (viewModel.hasChanges()) showRemoveConfirmation = true
-            else onFinish()
-        }
-    )
 
-    BackHandler(viewModel.hasChanges()) {
-        showRemoveConfirmation = true
+            if (accountId == null)
+                OutlinedTextField(
+                    value = state.balance,
+                    onValueChange = viewModel::onBalanceChanged,
+                    label = { Text("Баланс") },
+                    placeholder = { Text("10000 ₽") },
+                    modifier = Modifier.fillMaxWidth(),
+                    isError = state.balanceMessage != null,
+                    supportingText = { state.balanceMessage?.let { Text(it) } },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Decimal
+                    ),
+                    visualTransformation = CurrencyVisualTransformation()
+                )
+        }
     }
 
-    if (showRemoveConfirmation) {
+    BackHandler(viewModel.hasChanges()) {
+        showDiscardConfirmation = true
+    }
+
+    if (showDiscardConfirmation) {
         AlertDialog(
-            onDismissRequest = { showRemoveConfirmation = false },
+            onDismissRequest = { showDiscardConfirmation = false },
             title = { Text("Вы уверены?") },
             text = { Text("При закрытии изменения не будут сохранены") },
             confirmButton = {
                 TextButton(onClick = {
-                    showRemoveConfirmation = false
-                    onFinish()
+                    showDiscardConfirmation = false
+                    viewModel.onDismissClick()
                 }) { Text("Закрыть") }
             },
             dismissButton = {
-                TextButton(onClick = { showRemoveConfirmation = false }) {
+                TextButton(onClick = { showDiscardConfirmation = false }) {
                     Text("Отменить")
                 }
             })
--- app/app/src/main/java/com/denchic45/financetracker/ui/accounteditor/AccountEditorViewModel.kt
+++ app/app/src/main/java/com/denchic45/financetracker/ui/accounteditor/AccountEditorViewModel.kt
@@ -1,40 +1,164 @@
 package com.denchic45.financetracker.ui.accounteditor
 
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.denchic45.financetracker.Field
 import com.denchic45.financetracker.FieldEditor
 import com.denchic45.financetracker.api.account.model.AccountType
+import com.denchic45.financetracker.api.account.model.AdjustAccountBalanceRequest
+import com.denchic45.financetracker.api.account.model.CreateAccountRequest
+import com.denchic45.financetracker.api.account.model.UpdateAccountRequest
+import com.denchic45.financetracker.di.AppRouter
+import com.denchic45.financetracker.domain.usecase.AddAccountUseCase
+import com.denchic45.financetracker.domain.usecase.ObserveAccountByIdUseCase
+import com.denchic45.financetracker.domain.usecase.UpdateAccountUseCase
+import com.denchic45.financetracker.ui.AppEventHandler
+import com.denchic45.financetracker.ui.navigation.router.pop
+import com.denchic45.financetracker.ui.util.convertToAmount
+import com.denchic45.financetracker.ui.util.currencyRegex
+import com.denchic45.financetracker.ui.validator.CompositeValidator
+import com.denchic45.financetracker.ui.validator.Condition
+import com.denchic45.financetracker.ui.validator.Operator
+import com.denchic45.financetracker.ui.validator.ValueValidator
+import com.denchic45.financetracker.ui.validator.getIfNot
+import com.denchic45.financetracker.ui.validator.observable
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import java.util.UUID
 
-class AccountEditorViewModel(val accountId: Long?) : ViewModel() {
-    val state = EditableAccountState()
+class AccountEditorViewModel(
+    private val accountId: UUID?,
+    private val findAccountByIdUseCase: ObserveAccountByIdUseCase,
+    private val addAccountUseCase: AddAccountUseCase,
+    private val updateAccountUseCase: UpdateAccountUseCase,
+    private val appEventHandler: AppEventHandler,
+    private val router: AppRouter
+) : ViewModel() {
+
+    val state = EditingAccountState()
+
+
     private val fieldEditor = FieldEditor(
         mapOf(
             "name" to Field(state::name),
-            "type" to Field(state::type)
+            "type" to Field(state::type),
+            "balance" to Field(state::balance)
         )
     )
 
+    private val formValidator = CompositeValidator(
+        validators = listOf(
+            ValueValidator(
+                value = state::name,
+                conditions = listOf(
+                    Condition(String::isNotEmpty).observable { isValid ->
+                        state.nameMessage = getIfNot(isValid) { "Name is required" }
+                    }
+                )
+            ),
+            ValueValidator(
+                value = state::balance,
+                conditions = listOf(
+                    Condition(String::isNotEmpty).observable { isValid ->
+                        state.balanceMessage =
+                            getIfNot(isValid) { "Initial balance is required" }
+                    },
+                    Condition<String>({ it.toDoubleOrNull() != null }).observable { isValid ->
+                        if (!isValid && state.balance.isNotEmpty()) {
+                            state.balanceMessage = "Must be a number"
+                        }
+                    }
+                ),
+                operator = Operator.allEach()
+            )
+        ),
+        operator = Operator.allEach()
+    )
+
+    init {
+        viewModelScope.launch {
+            accountId?.let { id ->
+                findAccountByIdUseCase(id).first().getOrNull()?.let { account ->
+                    state.name = account.name
+                    state.type = account.type
+                    state.balance = account.balance.convertToAmount()
+                    fieldEditor.updateOldValues()
+                }
+            }
+        }
+    }
+
     fun onAccountNameChanged(name: String) {
         state.name = name
+        state.nameMessage = null
     }
 
     fun onAccountTypeChange(type: AccountType) {
         state.type = type
     }
 
-    fun onSaveClick() {
+    fun onBalanceChanged(balance: String) {
+        if (balance.matches(currencyRegex)) state.balance = balance
+        state.balanceMessage = null
+    }
 
+    fun onSaveClick() {
+        if (!formValidator.validate()) return
+
+        state.isLoading = true
+        viewModelScope.launch {
+            val result = if (accountId == null) {
+                addAccountUseCase(state.toCreateRequest())
+            } else {
+                updateAccountUseCase(
+                    accountId,
+                    state.toUpdateRequest(fieldEditor.fieldChanged("balance"))
+                )
+            }
+            state.isLoading = false
+            result.onLeft { failure -> appEventHandler.handleFailure(failure) }
+                .onRight { router.pop() }
+        }
     }
 
     fun hasChanges(): Boolean {
         return fieldEditor.hasChanges()
     }
+
+    fun onDismissClick() = router.pop()
 }
 
-class EditableAccountState {
+@Stable
+class EditingAccountState {
     var name by mutableStateOf("")
     var type by mutableStateOf(AccountType.CASH)
+    var balance by mutableStateOf("")
+    var createTransactionForUpdateBalance by mutableStateOf(false)
+    var nameMessage: String? by mutableStateOf(null)
+    var balanceMessage: String? by mutableStateOf(null)
+    var isLoading by mutableStateOf(false)
+
+    val realBalance: Long
+        get() = (balance.toFloat() * 100).toLong()
 }
+
+private fun EditingAccountState.toCreateRequest(): CreateAccountRequest {
+    return CreateAccountRequest(
+        name = name,
+        type = type,
+        initialBalance = realBalance,
+    )
+}
+
+private fun EditingAccountState.toUpdateRequest(balanceChanged: Boolean) = UpdateAccountRequest(
+    name = name,
+    type = type,
+    adjustBalance = if (balanceChanged) AdjustAccountBalanceRequest(
+        balance = realBalance,
+        createTransaction = createTransactionForUpdateBalance
+    ) else null,
+)
