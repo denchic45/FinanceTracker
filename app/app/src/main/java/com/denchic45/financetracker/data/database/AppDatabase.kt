1b2048e5887302007d4ac5cc5f4a5d925a8edf33
denchic45 <denchic150@gmail.com>
2025-11-16 22:59:07 +0300

    Add TagDao to AppDatabase

--- app/app/src/main/java/com/denchic45/financetracker/data/database/AppDatabase.kt
+++ app/app/src/main/java/com/denchic45/financetracker/data/database/AppDatabase.kt
@@ -4,17 +4,21 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.denchic45.financetracker.data.database.dao.AccountDao
 import com.denchic45.financetracker.data.database.dao.CategoryDao
+import com.denchic45.financetracker.data.database.dao.TagDao
 import com.denchic45.financetracker.data.database.dao.TransactionDao
 import com.denchic45.financetracker.data.database.entity.AccountEntity
 import com.denchic45.financetracker.data.database.entity.CategoryEntity
+import com.denchic45.financetracker.data.database.entity.TagEntity
 import com.denchic45.financetracker.data.database.entity.TransactionEntity
+import com.denchic45.financetracker.data.database.entity.TransactionTagCrossRef
 
 @Database(
     version = 1,
-    entities = [TransactionEntity::class, CategoryEntity::class, AccountEntity::class]
+    entities = [TransactionEntity::class, CategoryEntity::class, TagEntity::class, TransactionTagCrossRef::class, AccountEntity::class]
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract val transactionDao: TransactionDao
     abstract val categoryDao: CategoryDao
     abstract val accountDao: AccountDao
+    abstract val tagDao: TagDao
 }
