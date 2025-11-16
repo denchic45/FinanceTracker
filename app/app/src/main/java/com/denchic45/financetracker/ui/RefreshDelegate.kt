66dfa74ede94bcec66c9cdbc1ae5885face06bef
denchic45 <denchic150@gmail.com>
2025-11-16 21:48:01 +0300

    Add RefreshDelegate to create refreshable data flows

--- app/app/src/main/java/com/denchic45/financetracker/ui/RefreshDelegate.kt
+++ app/app/src/main/java/com/denchic45/financetracker/ui/RefreshDelegate.kt
@@ -1,11 +1,16 @@
 package com.denchic45.financetracker.ui
 
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.onStart
 
 class RefreshDelegate {
     private val _updateTrigger = Channel<Unit>(Channel.CONFLATED)
@@ -18,7 +23,30 @@ class RefreshDelegate {
         _isRefreshing.value = true
     }
 
+
     fun stopRefresh() {
         _isRefreshing.value = false
     }
 }
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun <T> RefreshDelegate.refreshableFlow(
+    block: suspend () -> Flow<T>
+) = updateTrigger.onStart { emit(Unit) }
+    .flatMapLatest { block() }
+    .onEach {
+        stopRefresh()
+    }
+
+@OptIn(ExperimentalCoroutinesApi::class)
+fun <T> refreshableFlow(
+    delegate1: RefreshDelegate,
+    delegate2: RefreshDelegate,
+    block: suspend () -> Flow<T>
+) = merge(delegate1.updateTrigger, delegate2.updateTrigger)
+    .onStart { emit(Unit) }
+    .flatMapLatest { block() }
+    .onEach {
+        delegate1.stopRefresh()
+        delegate2.stopRefresh()
+    }
