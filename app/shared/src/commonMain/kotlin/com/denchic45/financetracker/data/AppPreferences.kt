cbac52a5734d5cc3efbc334ee2d6559399106f35
denchic45 <denchic150@gmail.com>
2026-01-04 12:02:31 +0300

    Move Context.dataStore to android module

--- app/shared/src/commonMain/kotlin/com/denchic45/financetracker/data/AppPreferences.kt
+++ app/shared/src/commonMain/kotlin/com/denchic45/financetracker/data/AppPreferences.kt
@@ -1,14 +1,11 @@
 package com.denchic45.financetracker.data
 
-import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.map
 
-val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
 class AppPreferences(private val dataStore: DataStore<Preferences>) {
     private val accessTokenKey = stringPreferencesKey("token")
